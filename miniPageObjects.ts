cdc832580dd5c1433e80e637edf0441bfb139d4a
Jillian Salloum <salloum1@msu.edu>
2022-09-26 17:24:11 -0500

    first push

---
+++ miniPageObjects.ts
@@ -0,0 +1,10 @@
+import {BasePage} from './basePage.ts'
+import {By} from 'selenium-webdriver'
+
+export class Clicker extends BasePage {
+//Locators Here
+constructor() {
+super({url: 'https://orteil.dashnet.org/cookieclicker/'})
+}
+//methods
+}
